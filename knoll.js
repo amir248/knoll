f3e13442a82ac1b78097a9ad9e5c75b703495811
amir248 <chikchicly@yandex.ru>
2022-03-13 10:28:52 +0600

    if

--- knoll.js
+++ knoll.js
@@ -1,8 +1,8 @@
 console.log('pip');
-if(window.innerWidth<1100){
-  console.log('<1100');
-}else if(window.innerWidth<700){
-  console.log("<700");
+if(window.innerWidth<700){
+  console.log('<700');
+}else if(window.innerWidth<1100){
+  console.log("<1100");
 }else{
   console.log("else");
 }
